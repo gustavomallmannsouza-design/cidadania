8b478d88eae82b78598e0a20581a0addbc9edf4f
gustavomallmannsouza-design <gustavo.mallmann.souza@escola.pr.gov.br>
2026-06-23 11:24:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// Aguarda todo o conteúdo da página ser carregado
-window.addEventListener('DOMContentLoaded', () => {
-    
-    // Define um tempo de 3500 milissegundos (3.5 segundos) para a imagem sumir
-    setTimeout(() => {
-        const splash = document.getElementById('splash-screen');
-        
-        // Adiciona a classe CSS que esconde a tela com efeito de fade-out
-        splash.classList.add('hidden');
-        
-    }, 3500); 
-});
 

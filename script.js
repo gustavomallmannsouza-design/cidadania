7d9a15162e8dbb0a5a25878b651c03f1467f1237
gustavomallmannsouza-design <gustavo.mallmann.souza@escola.pr.gov.br>
2026-06-23 11:06:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function verificarResposta(isCorreto) {
+    const resultadoDiv = document.getElementById("resultado-quiz");
+    
+    if (isCorreto) {
+        resultadoDiv.innerHTML = "✅ Certinho! Mandou bem. Credenciais de jogos são pessoais e nunca devem ser compartilhadas com estranhos.";
+        resultadoDiv.className = "resultado correto";
+    } else {
+        resultadoDiv.innerHTML = "❌ Resposta incorreta! Cuidado. Esse é um golpe clássico de Phishing para roubar contas de jogos.";
+        resultadoDiv.className = "resultado incorreto";
+    }
+}
 

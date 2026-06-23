9d0d29790ecdbb614d68c20bfd1f22f3d45ce209
gustavomallmannsouza-design <gustavo.mallmann.souza@escola.pr.gov.br>
2026-06-23 11:25:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+    
+    // Aguarda o término da animação da barra de carregamento (3 segundos)
+    setTimeout(() => {
+        const splash = document.getElementById('splash-screen');
+        
+        // Adiciona a classe que faz a tela de splash desaparecer
+        splash.classList.add('hidden');
+        
+    }, 3200); 
+});
 
